14cdb1a42962e058bd0d38a1c15f2b8c69027e00
HF <hannibal.foks@mail.ru>
2021-01-26 11:11:05 +0300

    Создана страница-компонент "Товар". В файл css добавлены стили для Стр-к "Товар".

---
+++ components/product.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Любой</title>
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:wght@400;600&family=Roboto:wght@400;700&display=swap"
+          rel="stylesheet">
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="/style.css">
+</head>
+<body>
+    <main class="main main_produkt">
+        <div class="Product-top">
+            <h1 class="Product-topH1 stile_Garamond-bolt">Кухня Надежда Белый страйп</h1>
+            <div class="Product-top__info">
+                <h2 class="Product-top__infoH2 stile_Robot-bolt">Цена 140 000 ₽</h2>
+                <div class="Product-top__info_text stile_Robot-bolt">
+                    латинице с начала XVI века. В то время некий безымянный печатник создал большую<br>
+                    коллекцию размеров и форм шрифтов, используя Lorem Ipsum для распечатки образцов. <br>
+                    Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагное время послужили публикация листов <br>
+                    Letraset с образцами Lorem Ipsum в 60-х годах и, в более недавнее время,<br>
+                </div>
+            </div>
+        </div>
+        <div class="Product-imags">
+            <img src="/imag/matras_prestizh_feliks.jpg" alt="">
+            <img src="/imag/krovat_mdf_No1.jpg" alt="">
+            <img src="/imag/alfa_3.jpg" alt="">
+            <img src="/imag/alfa_5.jpg" alt="">
+        </div>
+    </main>
+</body>
+</html>
+
