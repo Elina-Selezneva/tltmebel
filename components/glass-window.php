961c9694b46e67a42f257d7d3fa1a71a82d57379
HF <hannibal.foks@mail.ru>
2021-01-26 17:30:04 +0300

    Создана страница-компонент "Витражи" (+адаптивен). Добавлены 4 картинки в папку imag.

---
+++ components/glass-window.php
@@ -0,0 +1,98 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Любой</title>
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:wght@400;600&family=Roboto:wght@400;700&display=swap"
+          rel="stylesheet">
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="/style.css">
+    <script src="https://use.fontawesome.com/4b2b3c4e4b.js"></script>
+</head>
+<body>
+<main class="main_glass">
+    <div class="glass__top">
+        <h1 class="glass__top-H1 stile_Garamond-bolt">Каталог витражных рисунков для шкафов-купе</h1>
+    </div>
+    <div class="glass__allCatalog">
+        <a href="#" class="glass__item">
+            <div class="">
+                <h2 class="glass__itemTop stile_Robot-bolt ">Однодверные. Абстракция.</h2>
+                <div class="glass__itemImag popup">
+                    <img src="/imag/glass1.jpg" alt="">
+                </div>
+            </div>
+        </a>
+        <a href="#" class="glass__item">
+            <div class="">
+                <h2 class="glass__itemTop stile_Robot-bolt ">Однодверные. Абстракция.</h2>
+                <div class="glass__itemImag popup">
+                    <img src="/imag/glass1.jpg" alt="">
+                </div>
+            </div>
+        </a>
+        <a href="#" class="glass__item">
+            <div class="">
+                <h2 class="glass__itemTop stile_Robot-bolt ">Однодверные. Абстракция.</h2>
+                <div class="glass__itemImag popup">
+                    <img src="/imag/glass3.jpg" alt="">
+                </div>
+            </div>
+        </a>
+        <a href="#" class="glass__item">
+            <div class="">
+                <h2 class="glass__itemTop stile_Robot-bolt ">Однодверные. Абстракция.</h2>
+                <div class="glass__itemImag popup">
+                    <img src="/imag/glass2.jpg" alt="">
+                </div>
+            </div>
+        </a>
+        <a href="#" class="glass__item">
+            <div class="">
+                <h2 class="glass__itemTop stile_Robot-bolt ">Однодверные. Абстракция.</h2>
+                <div class="glass__itemImag popup">
+                    <img src="/imag/glass4.jpg" alt="">
+                </div>
+            </div>
+        </a>
+        <a href="#" class="glass__item">
+            <div class="">
+                <h2 class="glass__itemTop stile_Robot-bolt ">Однодверные. Абстракция.</h2>
+                <div class="glass__itemImag popup">
+                    <img src="/imag/glass4.jpg" alt="">
+                </div>
+            </div>
+        </a>
+        <a href="#" class="glass__item">
+            <div class="">
+                <h2 class="glass__itemTop stile_Robot-bolt ">Однодверные. Абстракция.</h2>
+                <div class="glass__itemImag popup">
+                    <img src="/imag/glass4.jpg" alt="">
+                </div>
+            </div>
+        </a>
+        <a href="#" class="glass__item">
+            <div class="">
+                <h2 class="glass__itemTop stile_Robot-bolt ">Однодверные. Абстракция.</h2>
+                <div class="glass__itemImag popup">
+                    <img src="/imag/glass4.jpg" alt="">
+                </div>
+            </div>
+        </a>
+
+    <div class="show overlay">
+        <div class="img-show">
+            <span><i class="fa fa-times-circle" aria-hidden="true"></i></span>
+            <img src="" alt="">
+        </div>
+    </div>
+    <script type="text/javascript" src="/practic_glass.js"></script>
+</main>
+</body>
+</html>
