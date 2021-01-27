cb44db9e121a687e64cf364d76e8bcdc10a17b8e
Professional <eli.pond21@mail.ru>
2021-01-27 15:13:42 +0300

    made sand-page.

---
+++ components/sand-img.php
@@ -0,0 +1,25 @@
+<div class="first-page__container ">
+    <div class="sand_img-calalog">
+        <div class="glass__top">
+            <h1 class="glass__top-H1 stile_Garamond-bolt">Каталог пескоструйных рисунков для шкафов-купе под заказ</h1>
+        </div>
+        <div class="container__sand-img wrap">
+            <img src="/sand_img_catalog/sand_img1.jpg" alt="" class="sand_img">
+            <img src="/sand_img_catalog/sand_img2.jpg" alt="" class="sand_img">
+            <img src="/sand_img_catalog/sand_img3.jpg" alt="" class="sand_img">
+            <img src="/sand_img_catalog/sand_img4.jpg" alt="" class="sand_img">
+            <img src="/sand_img_catalog/sand_img5.jpg" alt="" class="sand_img">
+            <img src="/sand_img_catalog/sand_img6.jpg" alt="" class="sand_img">
+            <img src="/sand_img_catalog/sand_img7.jpg" alt="" class="sand_img">
+            <img src="/sand_img_catalog/sand_img8.jpg" alt="" class="sand_img">
+            <img src="/sand_img_catalog/sand_img9.jpg" alt="" class="sand_img">
+            <img src="/sand_img_catalog/sand_img10.jpg" alt="" class="sand_img">
+            <img src="/sand_img_catalog/sand_img11.jpg" alt="" class="sand_img">
+            <img src="/sand_img_catalog/sand_img12.jpg" alt="" class="sand_img">
+            <img src="/sand_img_catalog/sand_img13.jpg" alt="" class="sand_img">
+            <img src="/sand_img_catalog/sand_img14.jpg" alt="" class="sand_img">
+            <img src="/sand_img_catalog/sand_img15.jpg" alt="" class="sand_img">
+            <img src="/sand_img_catalog/sand_img16.jpg" alt="" class="sand_img">
+        </div>
+    </div>
+</div>
