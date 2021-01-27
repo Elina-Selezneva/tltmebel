1577c0e366a8ed5fed70696e829706ac4583d170
Professional <eli.pond21@mail.ru>
2021-01-27 15:00:28 +0300

    made main-page.

---
+++ components/main-page.php
@@ -0,0 +1,71 @@
+<div class="first-page__container container_color_blue">
+    <div class="bloks6">
+        <div class="bloks6__title stile_Garamond-bolt">Мы предлагаем</div>
+        <hr class="blocks6__line">
+        <div class="bloks6__content flex wrap">
+            <a href="" class="bloks6__block bloks6__block_fon"><span
+                        class="blocks6__text text_color_white text_color_white_shadow">Модульный диван</span></a>
+            <a href="" class="bloks6__block bloks6__block_fon2"><span
+                        class="blocks6__text text_color_white text_color_white_shadow">Каталог пескоструйных рисунков</span>
+            </a>
+            <a href="" class="bloks6__block bloks6__block_fon3"><span
+                        class="blocks6__text text_color_white text_color_white_shadow">Беспружинные <br> матасы</span></a>
+            <a href="" class="bloks6__block bloks6__block_fon4"><span
+                        class="blocks6__text text_color_white text_color_white_shadow">Кухни</span></a>
+            <a href="" class="bloks6__block bloks6__block_fon5"><span
+                        class="blocks6__text text_color_white text_color_white_shadow">Шкафы-купе</span></a>
+            <a href="" class="bloks6__block bloks6__block_fon6"><span
+                        class="blocks6__text text_color_white text_color_white_shadow">Каталог витражных рисунков для шкафов-купе</span></a>
+        </div>
+    </div>
+</div>
+<div class="first-page__container flex wrap">
+    <div class="about-us">
+        <div class="about-us__left">
+            <div class="about-us__text">
+                За время работы на мебельном рынке России компания зарекомендовала себя как производителя качественной
+                продукции по доступным ценам. Все изделия имеют сертификаты соответствия Госстандарта России и
+                санитарно-гигенический сертификат Федеральной службы по надзору в сфере защиты прав потребителя. Эти
+                документы удостоверяют, что ся мебель соответствует ГОСТу РФ и является безопасной для Вашего здоровья.
+                <br>
+                <br>
+                Мы можем украсить Ваш дом рисунками на фасадных частях мебели из МДФа по спец заказу матрасы в кроватях
+                использовать как односторонние, так и двух сторонние , фаненое и металлическое на латах основание,
+                возможно исполнение тахта с ящиком для белья подьём вбок.
+            </div>
+            <a href="" class="about-us__button make_credit__button_margin button_yellow">Подробнее</a>
+        </div>
+    </div>
+    <div class="about-us about-us__img">
+    </div>
+</div>
+<!--
+    <div class="first-page__container container_color_blue">
+        <div class="bloks6">
+            <div class="bloks6__title stile_Garamond-bolt">Новинки</div>
+            <hr class="blocks6__line">
+            <div class="bloks6__content flex wrap">
+                <div class="bloks6__block"></div>
+                <div class="bloks6__block"></div>
+                <div class="bloks6__block"></div>
+                <div class="bloks6__block"></div>
+                <div class="bloks6__block"></div>
+                <div class="bloks6__block"></div>
+            </div>
+        </div>
+    </div>
+-->
+<div class="first-page__container">
+    <div class="make_cradit">
+        <div class="bloks6__title make_cradit__title_margin stile_Garamond-bolt">Оформление Кредита</div>
+        <hr class="blocks6__line">
+        <div class="make_credit__text make_credit__text_font">
+            Мы являемся партнёрами " АЛЬФА - БАНК " и " Хоум Кредит Банк". Теперь можно оформить любую покупку в кредит
+            <span class="make_credit__text_weight">на выгоднях условиях,</span> сроком от 6 месяцев до 2-х лет. Для
+            оформления заказа нам не нужно подтверждения Ваших доходов , а только нужен Ваш Паспорт Р.Ф. и любой второй
+            документ (СНИЛС или Водительские Права и т.д). Так же можно оформить без первоначального взноса! Будем рады
+            Вам! С Уважением И.П. "Ананченко Т.А"
+        </div>
+        <a href="" class="about-us__button make_credit__button_margin button_yellow">Подробнее</a>
+    </div>
+</div>
